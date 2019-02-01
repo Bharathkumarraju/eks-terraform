0243ccfb2355c23c2fcd053c2cc9f5014a84b755
Rosemary Wang <rosemarywang888@gmail.com>
2019-02-01 13:15:03 -0500

    Fix outputs for list of outputs.

--- outputs.tf
+++ outputs.tf
@@ -29,5 +29,5 @@ output "region" {
 
 output "alb_controller_role_arn" {
   description = "ALB Controller Role ARN."
-  value       = "${aws_iam_role.k8s_alb_controller_role.arn}"
+  value       = "${aws_iam_role.k8s_alb_controller_role.*.arn}"
 }
